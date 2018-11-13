22ab6dbbaee6ffdd49af00d23091db4b1bd04ccf
David J. Hammaker <davidjosh93@gmail.com>
2018-11-13 13:12:48 -0500

    Add view to handle 404 errors
    Returns '404.html'

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -14,5 +14,10 @@ def add():
     return render_template('add.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True, port='7700')
