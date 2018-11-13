c67ccfc2b18574e3da07a8ffa8959ff5fa6041c3
David J. Hammaker <davidjosh93@gmail.com>
2018-11-12 22:38:46 -0500

    Update the app's ability to display 'index.html'
    Correct the "src" references in index.html, and add filler text. Add
    render_template('index.html') to the view.

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(port='7700')
+    app.run(debug=True, port='7700')
