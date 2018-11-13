e561413f8edd3aa9f8a5286c8d2a282f1f552b63
David J. Hammaker <davidjosh93@gmail.com>
2018-11-13 16:28:35 -0500

    Add flashes that appear after entering a URL in 'add'
    Flash will appear on any page. Flashes are styled to stand out at the 
    top of the page.

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = b'\x97\x91yV\xa0\xd5\xdf\\\x1ad\xe3\xe5\x803\x01*\xd5\xa9\x9d]\xb0.\xeaX'
 
 
 @app.route('/')
@@ -13,6 +14,7 @@ def index():
 def add():
     if request.method == "POST":
         url = request.form['url']
+        flash("Stored bookmark '{}'".format(url))
         return redirect(url_for('index'))
     return render_template('add.html')
 
