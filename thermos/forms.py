bf614efd09fe36bc999479f1a55b29788ce6aedf
David J. Hammaker <davidjosh93@gmail.com>
2018-11-20 13:56:46 -0500

    Employ Jinja macro for form field rendering
    Substitutes field rendering in the form on 'add.html' with the
    'render_field' macro, which exists in the new 'form_macros.html'. The
    'forms.py' file has been updated to account for this change, and it now
    has new field labels.

--- thermos/forms.py
+++ thermos/forms.py
@@ -5,5 +5,19 @@ from wtforms.validators import DataRequired, url
 
 
 class BookmarkForm(Form):
-    url = URLField('url', validators=[DataRequired(), url()])
-    description = StringField('description')
+    url = URLField('The URL for your bookmark:', validators=[DataRequired(), url()])
+    description = StringField('Add an optional description:')
+
+    def validate(self):
+        if not self.url.data.startswith("http://") and not self.url.data.startswith("https://"):
+            print("http:// {}".format(self.url.data.startswith("http://")))
+            print("https:// {}".format(self.url.data.startswith("https://")))
+            self.url.data = "http://" + self.url.data
+
+        if not Form.validate(self):
+            return False
+
+        if not self.description.data:
+            self.description.data = self.url.data
+
+        return True
