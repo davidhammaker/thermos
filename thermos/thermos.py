c2c92fa625cb16e15331605466343d720bc28f31
David J. Hammaker <davidjosh93@gmail.com>
2018-11-20 16:55:56 -0500

    Turn off track notifications
    Track notifications will soon be depricated for performance reasons.
    Turning it off suppresses a deprication warning.

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -11,6 +11,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\x97\x91yV\xa0\xd5\xdf\\\x1ad\xe3\xe5\x803\x01*\xd5\xa9\x9d]\xb0.\xeaX'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 bookmarks = []
